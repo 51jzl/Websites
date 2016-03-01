cf05d129baaf0bfeea3a89ad08e264171985a6be
Leon <1561888111@qq.com>
2016-03-02 01:43:07 +0800

    网站初始化
    网站初始化

---
+++ Websites/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Websites.Startup))]
+namespace Websites
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
